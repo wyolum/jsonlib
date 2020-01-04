f666be8574002f7fecb3f1228325061013cc1dd3
Justin Shaw <wyojustin@gmail.com>
2020-01-04 12:29:31 -0500

    shuffling files

--- examples/json_org/json_org.ino
+++ examples/json_org/json_org.ino
@@ -1,37 +1,40 @@
 #include <jsonlib.h>
 
-json = String(
-"{
-    \"glossary\": {
-        \"title\": \"example glossary\",
-		\"GlossDiv\": {
-            \"title\": \"S\",
-			\"GlossList\": {
-                \"GlossEntry\": {
-                    \"ID\": \"SGML\",
-					\"SortAs\": \"SGML\",
-					\"GlossTerm\": \"Standard Generalized Markup Language\",
-					\"Acronym\": \"SGML\",
-					\"Abbrev\": \"ISO 8879:1986\",
-					\"GlossDef\": {
-                        \"para\": \"A meta-markup language, used to create markup languages such as DocBook.\",
-						\"GlossSeeAlso\": [\"GML\", \"XML\"]
-                    },
-					\"GlossSee\": \"markup\"
-                }
-            }
-        }
-    }
-}");
+String json = String(
+"{"
+"    \"glossary\": {"
+"        \"title\": \"example glossary\","
+"		\"GlossDiv\": {"
+"            \"title\": \"S\","
+"			\"GlossList\": {"
+"                \"GlossEntry\": {"
+"                    \"ID\": \"SGML\","
+"					\"SortAs\": \"SGML\","
+"					\"GlossTerm\": \"Standard Generalized Markup Language\","
+"					\"Acronym\": \"SGML\","
+"					\"Abbrev\": \"ISO 8879:1986\","
+"					\"GlossDef\": {"
+"                        \"para\": \"A meta-markup language, used to create markup languages such as DocBook.\","
+"						\"GlossSeeAlso\": [\"GML\", \"XML\"]"
+"                    },"
+"					\"GlossSee\": \"markup\""
+"                }"
+"            }"
+"        }"
+"    }"
+"}");
 
 void setup(){
-  /*
-  json.replatce(" ", "");
-  json.replatce("\t", "");
-  json.replatce("\n", "");
-  */
-  json = removeWhiteSpace(json);
-  Serial.println(jsonExtract(json, "glossary"))
+  Serial.begin(115200);
+  Serial.println("Simple json library offered by wyolum.com\n\n");
+  
+  json = jsonRemoveWhiteSpace(json);
+  String glossary = jsonExtract(json, "glossary");
+  String glossdiv = jsonExtract(glossary, "GlossDiv");
+  String glossentry = jsonExtract(glossdiv, "GlossEntry");
+  String glossterm = jsonExtract(glossentry, "GlossTerm");
+  Serial.print("GlossTerm:");
+  Serial.println(glossterm);
 }
 void loop(){
 }
